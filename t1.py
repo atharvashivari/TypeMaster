157d2ba257e4892a283f5f41cbc4d815e517fd54
Atharva Shivari <137673478+atharvashivari@users.noreply.github.com>
2025-05-08 09:28:27 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,1 +1,14 @@
-print("hello")
+def sort(arr):
+    for i in range(len(arr)):
+        min_val = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_val]:
+                min_val = j
+
+        arr[i], arr[min_val] = arr[min_val], arr[i]
+
+    return arr
+
+data = [25,10,5,89,36]
+
+print(sort(data))
